5d350b0dbfc24ab64362cb5dc6c2829df90714ea
gshelia12 <gshelia.gsh@gmail.com>
2026-09-25 03:36:10 +0400

    Update EstatePhoto.jsx

--- react-app-ka/src/components/EstatePhoto.jsx
+++ react-app-ka/src/components/EstatePhoto.jsx
@@ -5,7 +5,7 @@ export default function EstatePhoto() {
   return (
     <section className="photo-section" data-screen-label="Estate photo">
       <figure className="framed">
-        <img src="/photos/chavchavadze-house.jpg" alt={t.photo.alt} style={{ objectPosition: 'center 45%' }} />
+        <img src={import.meta.env.BASE_URL + 'photos/chavchavadze-house.jpg'} alt={t.photo.alt} style={{ objectPosition: 'center 45%' }} />
         <figcaption>{t.photo.caption}</figcaption>
       </figure>
     </section>
