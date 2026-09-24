bf293d3438d36ea914686426145e10e4fdf54429
gshelia12 <gshelia.gsh@gmail.com>
2026-09-25 03:18:28 +0400

    Update EstatePhoto.jsx

--- react-app-ka/src/components/EstatePhoto.jsx
+++ react-app-ka/src/components/EstatePhoto.jsx
@@ -1,11 +1,12 @@
 import { useT } from '../i18n/LanguageContext.jsx'
+const houseUrl = import.meta.env.BASE_URL + 'photos/chavchavadze-house.jpg'
 
 export default function EstatePhoto() {
   const t = useT()
   return (
     <section className="photo-section" data-screen-label="Estate photo">
       <figure className="framed">
-        <img src="/photos/chavchavadze-house.jpg" alt={t.photo.alt} style={{ objectPosition: 'center 45%' }} />
+        <img src={houseUrl} alt={t.photo.alt} style={{ objectPosition: 'center 45%' }} />
         <figcaption>{t.photo.caption}</figcaption>
       </figure>
     </section>
