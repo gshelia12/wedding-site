50cd402d04645441e433e1e8ad0c95b414e0087f
gshelia12 <gshelia.gsh@gmail.com>
2026-09-25 03:33:27 +0400

    Update Travel.jsx

--- react-app-ka/src/components/Travel.jsx
+++ react-app-ka/src/components/Travel.jsx
@@ -6,7 +6,7 @@ export default function Travel() {
   return (
     <Section id="travel" label="Travel" kicker={t.travel.kicker} title={t.travel.title}>
       <figure className="travel-photo">
-        <img src="/photos/estate-gardens.jpg" alt={t.travel.alt} style={{ objectPosition: '70% 55%' }} />
+        <img src={import.meta.env.BASE_URL + 'photos/estate-gardens.jpg'} alt={t.travel.alt} style={{ objectPosition: '70% 55%' }} />
       </figure>
       <div className="travel-grid">
         {t.travel.cards.map(c => (
